75cef43dd026bf3aac402068a8f00d6edade675b
Aymen Jelassi <33674133+aymen-jelassi@users.noreply.github.com>
2022-06-08 01:30:16 +0000

    w

--- resources/[jobs]/caue-eblips/server/sv_emergency.lua
+++ resources/[jobs]/caue-eblips/server/sv_emergency.lua
@@ -1,44 +1,44 @@
 local EmergencyPlayers = {}
 
 RegisterNetEvent("e-blips:updateBlips", function(pJob, pSelfUpdate)
-    local src = source
+	local src = source
 
-    if exports["caue-jobs"]:getJob(pJob, "is_emergency") then
-        local callSign = exports["caue-jobs"]:getCallsign(src, pJob)
+	if exports["caue-jobs"]:getJob(pJob, "is_emergency") then
+		local callSign = exports["caue-jobs"]:getCallsign(src, pJob)
 
-        for k, v in pairs(EmergencyPlayers) do
-            TriggerClientEvent("e-blips:deleteHandlers", k)
-        end
+		for k, v in pairs(EmergencyPlayers) do
+			TriggerClientEvent("e-blips:deleteHandlers", k)
+		end
 
-        EmergencyPlayers[src] = {
-            netId = src,
-            job = pJob,
-            callsign = callSign or "CALLSIGN NOT DEFINED"
-        }
+		EmergencyPlayers[src] = {
+			netId = src,
+			job = pJob,
+			callsign = callSign or "CALLSIGN NOT DEFINED",
+		}
 
-        Citizen.Wait(1000)
+		Citizen.Wait(1000)
 
-        for k, v in pairs(EmergencyPlayers) do
-            TriggerClientEvent("e-blips:setHandlers", k, EmergencyPlayers)
-        end
-    elseif EmergencyPlayers[src] then
-        EmergencyPlayers[src] = nil
-        TriggerClientEvent("e-blips:deleteHandlers", src)
+		for k, v in pairs(EmergencyPlayers) do
+			TriggerClientEvent("e-blips:setHandlers", k, EmergencyPlayers)
+		end
+	elseif EmergencyPlayers[src] then
+		EmergencyPlayers[src] = nil
+		TriggerClientEvent("e-blips:deleteHandlers", src)
 
-        for k, v in pairs(EmergencyPlayers) do
-            TriggerClientEvent("e-blips:removeHandler", k, src)
-        end
-    end
+		for k, v in pairs(EmergencyPlayers) do
+			TriggerClientEvent("e-blips:removeHandler", k, src)
+		end
+	end
 end)
 
 AddEventHandler("playerDropped", function()
 	local src = source
 
-    if EmergencyPlayers[src] then
-        EmergencyPlayers[src] = nil
+	if EmergencyPlayers[src] then
+		EmergencyPlayers[src] = nil
 
-        for k, v in pairs(EmergencyPlayers) do
-            TriggerClientEvent("e-blips:removeHandler", k, src)
-        end
-    end
+		for _, v in pairs(EmergencyPlayers) do
+			TriggerClientEvent("e-blips:removeHandler", v, src)
+		end
+	end
 end)
