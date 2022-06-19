4f9cc1908cb40a45f474be582e5e143345b17066
Aymen Jelassi <33674133+aymen-jelassi@users.noreply.github.com>
2022-06-19 22:10:50 +0000

    translation bicycles

--- resources/[vehicles]/caue-bicycles/client/cl_init.lua
+++ resources/[vehicles]/caue-bicycles/client/cl_init.lua
@@ -9,7 +9,7 @@ Citizen.CreateThread(function()
     local data = {
         {
             id = "bicycle_shop",
-            label = "Loja de Bikes",
+            label = "Bike Shop",
             icon = "bicycle",
             event = "caue-bicycles:showBicycles",
             parameters = {},
