3d316825531e12fe6ec5b0adc7f6d389988f3f9f
Aymen Jelassi <33674133+aymen-jelassi@users.noreply.github.com>
2022-06-21 10:41:43 +0000

    format

--- resources/[login]/caue-login/server/sv_char.lua
+++ resources/[login]/caue-login/server/sv_char.lua
@@ -1,134 +1,144 @@
 local function phoneNumber()
-    for i = 1, 10 do
-        local areaCode = math.random(50) > 25 and 415 or 628
-        local number = math.random(1000000, 9999999)
+	for i = 1, 10 do
+		local areaCode = math.random(50) > 25 and 415 or 628
+		local number = math.random(1000000, 9999999)
 
-        local phoneNumber = tonumber(areaCode .. number)
+		local phoneNumber = tonumber(areaCode .. number)
 
-        local exist = MySQL.scalar.await([[
+		local exist = MySQL.scalar.await(
+			[[
             SELECT phone
             FROM characters
             WHERE phone = ?
         ]],
-        { phoneNumber })
+			{ phoneNumber }
+		)
 
-        if not exist then
-            return phoneNumber
-        end
+		if not exist then
+			return phoneNumber
+		end
 
-        Citizen.Wait(500)
-    end
+		Citizen.Wait(500)
+	end
 end
 
 RegisterNetEvent("login:getCharModels")
 AddEventHandler("login:getCharModels", function(charIds, isReset)
-    local src = source
+	local src = source
 
-    local result = MySQL.query.await([[
+	local result = MySQL.query.await([[
         SELECT *
         FROM characters_clothes
         WHERE cid IN (]] .. charIds .. [[)
     ]])
 
-    local peds = {}
+	local peds = {}
 
-    for i,v in ipairs(result) do
-        peds[v.cid] = {
-            model = v.model,
-            drawables = json.decode(v.drawables),
-            props = json.decode(v.props),
-            drawtextures = json.decode(v.drawtextures),
-            proptextures = json.decode(v.proptextures),
-            hairColor = json.decode(v.hairColor),
-            headBlend = json.decode(v.headBlend),
-            headStructure = json.decode(v.headStructure),
-            headOverlay = json.decode(v.headOverlay),
-            tattoos = json.decode(v.tattoos)
-        }
-    end
+	for i, v in ipairs(result) do
+		peds[v.cid] = {
+			model = v.model,
+			drawables = json.decode(v.drawables),
+			props = json.decode(v.props),
+			drawtextures = json.decode(v.drawtextures),
+			proptextures = json.decode(v.proptextures),
+			hairColor = json.decode(v.hairColor),
+			headBlend = json.decode(v.headBlend),
+			headStructure = json.decode(v.headStructure),
+			headOverlay = json.decode(v.headOverlay),
+			tattoos = json.decode(v.tattoos),
+		}
+	end
 
-    TriggerClientEvent("login:CreatePlayerCharacterPeds", src, peds, isReset)
+	TriggerClientEvent("login:CreatePlayerCharacterPeds", src, peds, isReset)
 end)
 
 RPC.register("caue-char:fetchCharacters", function(src)
-    local ids = GetIds(src)
+	local ids = GetIds(src)
 
-    if not ids.hex then
-        return {
-            err = "Seu hex não foi encontrado"
-        }
-    end
+	if not ids.hex then
+		return {
+			err = "Your hex is not found",
+		}
+	end
 
-    local characters = MySQL.query.await([[
+	local characters = MySQL.query.await(
+		[[
         SELECT *
         FROM characters
         WHERE hex = ? AND deleted = 0
     ]],
-    { ids.hex })
+		{ ids.hex }
+	)
 
-    return characters
+	return characters
 end)
 
 RPC.register("caue-char:createCharacter", function(src, params)
-    local ids = GetIds(src)
+	local ids = GetIds(src)
 
-    if not ids.hex then
-        return {
-            err = "Seu hex não foi encontrado"
-        }
-    end
+	if not ids.hex then
+		return {
+			err = "Your hex is not found",
+		}
+	end
 
-    local function formatDate(s)
-        local y, m, d = string.match(s,  '^(%d%d%d%d)-(%d%d)-(%d%d)$' )
-        return "" .. d .. "/" .. m .. "/" .. y .. ""
-    end
+	local function formatDate(s)
+		local y, m, d = string.match(s, "^(%d%d%d%d)-(%d%d)-(%d%d)$")
+		return "" .. d .. "/" .. m .. "/" .. y .. ""
+	end
 
-    local hex = ids.hex
-    local fname = ((params.firstname):lower()):gsub("^%l", string.upper)
-    local lname = ((params.lastname):lower()):gsub("^%l", string.upper)
-    local gender = params.gender
-    local dob = formatDate(params.dob)
-    local phone = phoneNumber()
+	local hex = ids.hex
+	local fname = ((params.firstname):lower()):gsub("^%l", string.upper)
+	local lname = ((params.lastname):lower()):gsub("^%l", string.upper)
+	local gender = params.gender
+	local dob = formatDate(params.dob)
+	local phone = phoneNumber()
 
-    local insertId = MySQL.insert.await([[
+	local insertId = MySQL.insert.await(
+		[[
         INSERT INTO characters (hex, first_name, last_name, gender, dob, phone)
         VALUES (?, ?, ?, ?, ?, ?)
     ]],
-    { hex, fname, lname, gender, dob, phone })
+		{ hex, fname, lname, gender, dob, phone }
+	)
 
-    exports["caue-logs"]:AddLog("character-create", src, insertId)
+	exports["caue-logs"]:AddLog("character-create", src, insertId)
 
-    return {}
+	return {}
 end)
 
 RPC.register("caue-char:deleteCharacter", function(src, cid)
-    MySQL.update.await([[
+	MySQL.update.await(
+		[[
         UPDATE characters
         SET deleted = 1
         WHERE id = ?
     ]],
-    { cid })
+		{ cid }
+	)
 
-    exports["caue-logs"]:AddLog("character-delete", src, cid)
+	exports["caue-logs"]:AddLog("character-delete", src, cid)
 
-    return {}
+	return {}
 end)
 
 RPC.register("caue-char:selectCharacter", function(src, cid)
-    local character = MySQL.single.await([[
+	local character = MySQL.single.await(
+		[[
         SELECT id, hex, first_name, last_name, gender, dob, cash, bankid, phone, job, job2, jail, new, hotel, aliases
         FROM characters
         WHERE id = ?
     ]],
-    { cid })
+		{ cid }
+	)
 
-    if not character then
-        return nil
-    end
+	if not character then
+		return nil
+	end
 
-    character["phone"] = math.ceil(character["phone"])
+	character["phone"] = math.ceil(character["phone"])
 
-    exports["caue-base"]:setUser(src, "char", character)
+	exports["caue-base"]:setUser(src, "char", character)
 
-    return character
+	return character
 end)
